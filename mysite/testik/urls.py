9c1a73f5ed2f7718134f18d972e789ecede357a3
Nikita Marin <nikita18.marin@gmail.com>
2022-07-02 16:10:12 +0300

    make bad href

--- mysite/testik/urls.py
+++ mysite/testik/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main_page, description_test, history, result, passing_test, tests_page
+from .views import main_page, description_test, history, result, passing_test, tests_page, get_category
 
 urlpatterns = [
     path('', main_page),
     path('tests', tests_page),
+    path('tests/category/<int:category_id>/', get_category),
     path('tests/description/', description_test),
     path('tests/passing/', passing_test),
     path('tests/result/', result),
--- mysite/testik/views.py
+++ mysite/testik/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Test
+from .models import Test, Category
 
 
 def main_page(request):
@@ -9,8 +9,25 @@ def main_page(request):
 
 def tests_page(request):
     all_tests = Test.objects.all()
-    return render(request, 'testik/tests_page.html', {'all_tests': all_tests, 'title': 'наши тесты'})
-    # return HttpResponse('Тут все тесты')
+    categories = Category.objects.all()
+    context = {
+        'all_tests': all_tests,
+        'categories': categories,
+        'title': 'Наши тесты'
+    }
+    return render(request, template_name='testik/tests_page.html', context=context)
+
+
+def get_category(request, category_id):
+    all_tests = Test.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    curr_category = Category.objects.get(pk=category_id)
+    context = {
+        'all_tests': all_tests,
+        'categories': categories,
+        'category': curr_category
+    }
+    return render(request, template_name='testik/category.html', context=context)
 
 
 def description_test(request):
