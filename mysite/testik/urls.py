3ee8f9e07572ed68b096d56e4ae4e72c722317b7
Nikita Marin <nikita18.marin@gmail.com>
2022-07-03 02:46:37 +0300

    add base.htm and correct urls,tests pages

--- mysite/testik/urls.py
+++ mysite/testik/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import main_page, description_test, history, result, passing_test, tests_page, get_category
 
 urlpatterns = [
-    path('', main_page),
-    path('tests', tests_page),
-    path('tests/category/<int:category_id>/', get_category),
-    path('tests/description/', description_test),
-    path('tests/passing/', passing_test),
-    path('tests/result/', result),
-    path('history/', history)
+    path('', main_page, name='main'),
+    path('tests', tests_page, name='tests'),
+    path('tests/category/<int:category_id>/', get_category, name='category'),
+    path('tests/description/', description_test, name='description'),
+    path('tests/passing/', passing_test, name='passing'),
+    path('tests/result/', result, name='result'),
+    path('history/', history, name='history')
 ]
